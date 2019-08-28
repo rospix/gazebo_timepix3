64483bee72fc7cd1af97f3a6ff1bd3d3f89c5347
Petr Štibinger <stibipet@fel.cvut.cz>
2019-08-28 15:15:36 +0200

    better interpolation of material coefficients

--- include/materials.h
+++ include/materials.h
@@ -21,19 +21,19 @@ public:
 /* Material library //{ */
 
 // Elements
-const static Material Si   = Material("Si", 2.329, 1.3749E-01, 2.951E-02);
-const static Material Cd   = Material("Cd", 8.65, 5.9900E+00, 3.339E-02);
+const static Material Si   = Material("Si", 2.329, 0.325031909908, 0.0772599124249);
+const static Material Cd   = Material("Cd", 8.65, 6.15220721228, 0.0752410682746);
+const static Material Te   = Material("Te", 6.24, 7.0963E+00, 3.440E-02);
+const static Material CdTe = Material("CdTe", 6.2, 6.66938158906, 0.0747982314383);
+const static Material C    = Material("C", 1.7, 0.175791379779, 0.0771243158107);
 const static Material Na   = Material("Na", 0.968, 5.5004E-02, 2.830E-02);
 const static Material I    = Material("I", 4.933, 7.6433E+00, 3.598E-02);
-const static Material Te   = Material("Te", 6.24, 7.0963E+00, 3.440E-02);
-const static Material CdTe = Material("CdTe", 5.85, 4.163E+00, 3.391E-02);
-const static Material C    = Material("C", 1.7, 1.753E-01, 8.058E-02);
 
 // Compounds
 const static Material concrete = Material("concrete", 2.3, 2.66E-01, 6.236E-02);
 const static Material wood     = Material("wood", 0.7, 1.907E-01, 8.070E-02);
 const static Material glass    = Material("glass", 2.23, 2.417E-01, 8.035E-02);
-const static Material air      = Material("air", 1.205E-3, 1.875E-01, 8.055E-02);
+const static Material air      = Material("air", 1.205E-3, 0.188309591009, 0.0770994336217);
 const static Material water    = Material("water", 1.0, 2.059E-01, 8.956E-02);
 //}
 
